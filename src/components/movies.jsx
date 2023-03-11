4c123032be6d40f66c3a000de960e0a5911b3a02
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2023-03-11 12:35:21 +0100

    migrated class component to function ones

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,56 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { getMovies } from "../services/fakeMovieService";
 
-class Movies extends Component {
-  state = { movies: getMovies() };
+const Movies = () => {
+  const [movies, setMovies] = useState(getMovies());
 
-  handleDelete = (movie) => {
-    const movies = this.state.movies.filter((m) => m._id !== movie._id);
-    this.setState({ movies });
+  const handleDelete = (movie) => {
+    setMovies(movies.filter((m) => m._id !== movie._id));
   };
 
-  render() {
-    const { length: count } = this.state.movies;
-    const { movies } = this.state;
+  const { length: count } = movies;
 
-    if (count === 0) return <p>There are no movies in the database</p>;
+  if (count === 0) return <p>There are no movies in the database</p>;
 
-    return (
-      <>
-        <p>Showing {count} movies in the database.</p>
+  return (
+    <>
+      <p>Showing {count} movies in the database.</p>
 
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th>&nbsp;</th>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    handleDelete(movie);
+                  }}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default Movies;
